34e04ecfa1d0a3f72a8d34416f42963a0b78b31a
cliffordcoco <88732726+nage2285@users.noreply.github.com>
2021-08-10 16:49:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random_int = random.randint(0,1)
+#Heads = str("0")
+#Tails = str("1")
+
+#random_int = str(random_int)
+#1 == "Heads"
+#0 == "Tails"
+#random_int = str(random_int)
+
+if random_int == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+#random_int = str(random_int("Heads","Tails"))
+#print(random_int) //Anyways, it was printing above//
+
+
+
+
+
+
+
+
+
